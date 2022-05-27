300300e454664d7caf6e9f60647f41719cf10fb0
cossic011 <84376210+cossic011@users.noreply.github.com>
2022-05-26 21:25:16 -1100

    fixed banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -18,9 +18,6 @@ interface Props {
 const Banner = ({ netflixOriginals }: Props) => {
   const [movie, setMovie] = useState<Movie | null>(null)
 
-  const { modalState: showModal } = useSelector(
-    (state: RootState) => state.modal
-  )
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -33,7 +30,7 @@ const Banner = ({ netflixOriginals }: Props) => {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
-          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path} `}
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
         />
